523336930743414552beae15700c286f327d36db
Andrii Pukhalyk <pukhalyk1@gmail.com>
2024-07-03 11:05:24 -0600

    Installed lsp for rust
    Added rustcea, installed rust-analyzer with Mason
    and disabled rust-analyzer to automatically attach to rust files

---
+++ .config/nvim/lua/plugins/rustacea.lua
@@ -0,0 +1,16 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+--- https://github.com/mrcjkb/rustaceanvim
+
+---@type LazySpec
+return {
+  {
+    "mrcjkb/rustaceanvim",
+    -- config = function()
+    --   require("rustacean").setup {
+    --     -- config
+    --   }
+    -- end,
+    lazy = false,
+  },
+}
