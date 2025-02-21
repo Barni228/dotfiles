d552f0e3e5878d574bcc6bedd10f78708fb733be
Andrii Pukhalyk <pukhalyk1@gmail.com>
2025-02-20 22:07:46 -0700

    Nvim, added emmet plugin
    now i can press <C-h> to expand emmet snippet (html code)

--- .config/nvim/lua/plugins/emmet.lua
+++ .config/nvim/lua/plugins/emmet.lua
@@ -3,38 +3,26 @@
 --- https://github.com/mattn/emmet-vim
 
 -- disable defaults
--- vim.g.user_emmet_install_global = 0
-
--- only enable for insert mode
-vim.g.user_emmet_mode = 'i'
-
--- pressing <Ctrl+h> to expand emmet abbreviation
--- div>h1|
--- Ctrl h
--- <div>
---   <h1>|</h1>
--- </div>
-vim.g.user_emmet_expandabbr_key = '<C-h>'
-
--- disable all other mappings
-vim.g.user_emmet_expandword_key = '<Nop>'
-vim.g.user_emmet_update_tag = '<Nop>'
-vim.g.user_emmet_balancetaginward_key = '<Nop>'
-vim.g.user_emmet_balancetagoutward_key = '<Nop>'
-vim.g.user_emmet_next_key = '<Nop>'
-vim.g.user_emmet_prev_key = '<Nop>'
-vim.g.user_emmet_imagesize_key = '<Nop>'
-vim.g.user_emmet_togglecomment_key = '<Nop>'
-vim.g.user_emmet_splitjointag_key = '<Nop>'
-vim.g.user_emmet_removetag_key = '<Nop>'
-vim.g.user_emmet_anchorizeurl_key = '<Nop>'
-vim.g.user_emmet_anchorizesummary_key = '<Nop>'
-vim.g.user_emmet_mergelines_key = '<Nop>'
-vim.g.user_emmet_codepretty_key = '<Nop>'
+vim.g.user_emmet_install_global = 0
 
 ---@type LazySpec
 return {
   {
     "mattn/emmet-vim",
+
+    config = function()
+      -- pressing <Ctrl+h> to expand emmet abbreviation
+      -- div>h1|
+      -- Ctrl h
+      -- <div>
+      --   <h1>|</h1>
+      -- </div>
+      vim.keymap.set(
+        "i",
+        "<C-h>",
+        "<c-r>=emmet#util#closePopup()<cr><c-r>=emmet#expandAbbr(0,'')<cr>",
+        { silent = true, desc = "expand emmet abbreviation" }
+      )
+    end,
   },
 }
