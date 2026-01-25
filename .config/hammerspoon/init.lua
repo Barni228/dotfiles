dcae5b5ef4fbcad7e0d159fcf6fe606e48d81563
Andrii Pukhalyk <pukhalyk1@gmail.com>
2026-01-24 23:00:08 -0700

    Hammerspoon, move config path to ~/.config
    defaults write org.hammerspoon.Hammerspoon MJConfigFile
    "~/.config/hammerspoon/init.lua"
    Is the command to change hammerspoon config location

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,3 +1,6 @@
+-- run this command to have hammerspoon in .config folder (default is ~/.hammerspoon)
+-- defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
+
 -- this will add a `clear` command to hammerspoon console, so i don't have to type `hs.console.clearConsole()`
 clear = hs.console.clearConsole
 
